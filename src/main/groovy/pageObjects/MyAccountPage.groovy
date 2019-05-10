afacedcd74078d0e14fb854a8c6fd535b0c27b75
tonyo.zhelev <tonyo_zhelev@abv.bg>
2019-05-10 14:52:18 +0300

    My Account page added all elements

--- src/main/groovy/pageObjects/MyAccountPage.groovy
+++ src/main/groovy/pageObjects/MyAccountPage.groovy
@@ -1,10 +1,17 @@
 package pageObjects
 
 import geb.Page
+import org.openqa.selenium.By
 
 class MyAccountPage extends Page {
     static at = {waitFor{mainHeading.isDisplayed()}}
     static content = {
         mainHeading {$("h1.page-heading", text: "MY ACCOUNT")}
+        orderHistory {$(By.xpath("//a[@title=\"Orders\"]"))}
+        creditSlips {$(By.xpath("//a[@title=\"Credit slips\"]"))}
+        addresses {$(By.xpath("//a[@title=\"Addresses\"]"))}
+        personalInformation {$(By.xpath("//a[@title=\"Information\"]"))}
+        wishlist {$(By.xpath("//a[@title=\"My wishlist\"]"))}
+        homeButton {$(By.xpath("//a[@title=\"Home\"]"))}
     }
 }
--- src/test/groovy/TestingSpec.groovy
+++ src/test/groovy/TestingSpec.groovy
@@ -1,3 +1,4 @@
+import geb.Page
 import pageObjects.AuthenticationPage
 import pageObjects.CreateAccountPage
 import pageObjects.IndexPage
@@ -6,6 +7,11 @@ import pageObjects.MyAccountPage
 
 //tests here are designed only to verify that the Page Object properties are working as intended
 class TestingSpec extends BaseSpec {
+    private void successfulLogin()
+    {
+        login("fadsfs@fdsf.dsa", "asdfgqwer")
+    }
+
     void signinClickTest(){
         given:
         to IndexPage
@@ -21,25 +27,35 @@ class TestingSpec extends BaseSpec {
         given:
         to AuthenticationPage
         when:
-        login("fadsfs@fdsf.dsa", "asdfgqwer")
+        successfulLogin()
         then:
         at MyAccountPage
     }
 
-    void radioTest(){
+    void createAccountPageTest(){
         given:
         to AuthenticationPage
         when:
         createAccount("test123@test123.test1")
-        CreateAccountPage newPage = at CreateAccountPage
-        //TODO: da pitam iliq kak da go deklariram direktno v samiq class
+        Page newPage = at CreateAccountPage
         newPage.radioGender = "Mrs."
         newPage.selectDays.selected = '2'
         newPage.checkboxNewsletter.check()
         then:
-        newPage.radioGender.value() == '2'
+        newPage.radioGender == "2"
         newPage.selectDays.selectedText.trim() == '2'
         newPage.checkboxNewsletter.checked
+    }
 
+    void myAccountPageTest(){
+        given:
+        to AuthenticationPage
+        successfulLogin()
+        when:
+        Page newPage = at MyAccountPage
+        newPage.orderHistory.click()
+        def heading = waitFor {$("h1.page-heading", text: "ORDER HISTORY")}
+        then:
+        heading.isDisplayed()
     }
 }
